f5c7d3913b642acb298727cf1db936a5e7bedd92
ankurThakur1 <ankurdev.react+github@gmail.com>
2025-03-16 09:59:47 +0530

    login functionality added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -57,7 +57,50 @@ const registerUser = async (req, res) => {
 }
 
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
+        if(!email || !password){
+            res.status(400).json({
+                message: "email and password are required",
+                success: false
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(400).json({
+                message: "User doesn't exists in our database!! Please register first",
+                success: false
+            });
+        }
+
+        const comparePassword = await bcrypt.compare(password, user.password);
+
+        if(!comparePassword){
+            return res.status(404).json({
+                message: "Invalid Credentials",
+                success: false
+            });
+        }
+
+        const token = jwt.sign({userId: user._id}, process.env.AUTH_SECRET, { expiresIn: "1d" });
+
+        return res.status(200).json({
+            message: "User logged in successfully...",
+            success: true,
+            userData: user,
+            accessToken: token
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error in register",
+            success: false
+        });
+    }
 }
 
 module.exports = { registerUser, loginUser }
